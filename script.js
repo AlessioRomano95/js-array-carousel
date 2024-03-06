a707981abd7f19284fc32b9285eb027c44273bdb
AlessioRomano95 <romanalex400@gmail.com>
2024-03-06 15:16:30 +0100

    [ADD] css e const

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log('js ok')
+
+// 1
+const itemsWrapper = document.querySelector('.items-wrapper');
+const topC = document.querySelector('.top');
+const botC = document.querySelector('.bottom');
+
+topC.classList.add('hide')
+
+// 2
+
+const images = [
+  'asset/img/01.webp',
+  'asset/img/02.webp',
+  'asset/img/03.webp',
+  'asset/img/04.webp',
+  'asset/img/05.webp',
+]
+
+let counterImg = 0;
